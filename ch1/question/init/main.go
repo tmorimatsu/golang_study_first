f41f5f8f6a373677a340fd9925ba35fff1063110
mless <>
2018-07-12 23:22:19 +0900

    add question

---
+++ ch1/question/init/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+}
+
+func init() {
+	fmt.Println("test0")
+}
+
+func init() {
+	fmt.Println("test2")
+}
+
+func init() {
+	fmt.Println("test1")
+}
+
+func init() {
+	fmt.Println("test2")
+}
