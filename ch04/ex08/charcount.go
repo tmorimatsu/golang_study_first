d7b35e6d5c9adb0de05c7a6fe318e62e8b655f39
mless <>
2018-08-11 21:33:17 +0900

    l

---
+++ ch04/ex08/charcount.go
@@ -0,0 +1,43 @@
+package charcount
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"bufio"
+	"unicode"
+)
+
+func charcount() {
+	counts := make(amp[rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // rune, nbytes, error を返す
+		if err == io.EOF {
+			break;
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+}
