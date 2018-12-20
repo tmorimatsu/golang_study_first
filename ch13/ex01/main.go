78c3c19a4cd0c23f7208921bb0091b62496860a6
mless <>
2018-12-20 09:02:20 +0900

    add ch13/ex01

---
+++ ch13/ex01/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"math"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+
+}
+
+func almostEquals(v1, v2 float64) bool {
+
+	if v1 == v2 {
+		return true
+	}
+
+	diff := v1 - v2
+	if 0.00000001 > math.Abs(diff) {
+		return true
+	}
+	return false
+}
+
+func equal(x, y reflect.Value, seen map[comparison]bool) bool {
+	if !x.IsValid() || !y.IsValid() {
+		return x.IsValid() == y.IsValid()
+	}
+	if x.Type() != y.Type() {
+		return false
+	}
+
+	if x.CanAddr() && y.CanAddr() {
+		xptr := unsafe.Pointer(x.UnsafeAddr())
+		yptr := unsafe.Pointer(y.UnsafeAddr())
+		if xptr == yptr {
+			return true // identical references
+		}
+		c := comparison{xptr, yptr, x.Type()}
+		if seen[c] {
+			return true // already seen
+		}
+		seen[c] = true
+	}
+
+	switch x.Kind() {
+	case reflect.Bool:
+		return x.Bool() == y.Bool()
+
+	case reflect.String:
+		return x.String() == y.String()
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		return x.Int() == y.Int()
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+		reflect.Uint64, reflect.Uintptr:
+		return x.Uint() == y.Uint()
+
+	case reflect.Float32, reflect.Float64:
+		return almostEquals(float64(x.Float()), float64(y.Float()))
+
+	case reflect.Complex64, reflect.Complex128:
+		x := x.Complex()
+		y := y.Complex()
+		return almostEquals(float64(real(x)), float64(real(y))) && almostEquals(float64(imag(x)), float64(imag(y)))
+
+	case reflect.Chan, reflect.UnsafePointer, reflect.Func:
+		return x.Pointer() == y.Pointer()
+
+	case reflect.Ptr, reflect.Interface:
+		return equal(x.Elem(), y.Elem(), seen)
+
+	case reflect.Array, reflect.Slice:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for i := 0; i < x.Len(); i++ {
+			if !equal(x.Index(i), y.Index(i), seen) {
+				return false
+			}
+		}
+		return true
+
+	case reflect.Struct:
+		for i, n := 0, x.NumField(); i < n; i++ {
+			if !equal(x.Field(i), y.Field(i), seen) {
+				return false
+			}
+		}
+		return true
+
+	case reflect.Map:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for _, k := range x.MapKeys() {
+			if !equal(x.MapIndex(k), y.MapIndex(k), seen) {
+				return false
+			}
+		}
+		return true
+	}
+	panic("unreachable")
+}
+
+func Equal(x, y interface{}) bool {
+	seen := make(map[comparison]bool)
+	return equal(reflect.ValueOf(x), reflect.ValueOf(y), seen)
+}
+
+type comparison struct {
+	x, y unsafe.Pointer
+	t    reflect.Type
+}
